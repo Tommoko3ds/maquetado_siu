3b60b76e5b2b49afe95b16f4ac3dfbc0047e8a5e
SirMauricio <190300383@ucaribe.edu.mx>
2024-02-28 07:33:41 -0500

    Subir vista de presidente. Asesores y estudiantes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-export default function App() {
+// src/App.js
+import React, { useState } from 'react';
+import NavBar from './componentes/NavBar';
+import Sidebar from './componentes/Sidebar';
+import CardInfo from './componentes/CardInfo';
+
+const App = () => {
+  const [selectedCard, setSelectedCard] = useState(null);
+
+  const cards = [
+    { id: 1, title: 'Card 1', description: 'Descripción de la Card 1' },
+    { id: 2, title: 'Card 2', description: 'Descripción de la Card 2' },
+    { id: 3, title: 'Card 3', description: 'Descripción de la Card 3' },
+    { id: 4, title: 'Card 4', description: 'Descripción de la Card 4' },
+    { id: 5, title: 'Card 5', description: 'Descripción de la Card 5' },
+    // Agrega más cards según tus necesidades
+  ];
+
+  const handleSelectCard = (card) => {
+    setSelectedCard(card);
+  };
+
   return (
-    <h1 className="text-4xl font-bold text-pink-300">
-      Hello world!
-    </h1>
-  )
-}
+    <div>
+      <NavBar />
+      <div className="flex mt-20">
+        <Sidebar cards={cards} onSelectCard={handleSelectCard} />
+        <CardInfo selectedCard={selectedCard} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/componentes/Sidebar.jsx
@@ -0,0 +1,25 @@
+// src/componentes/Sidebar.jsx
+import React from 'react';
+
+const Sidebar = ({ cards, onSelectCard }) => {
+  return (
+    <div className="bg-gray-200 p-4 w-1/4 mb-10 ml-10"> {/* Agrega las clases 'mb-4' y 'ml-4' */}
+      <h2 className="text-xl font-bold mb-4">Cards</h2>
+
+      <div className="space-y-4">
+        {cards.map((card) => (
+          <div
+            key={card.id}
+            className="bg-white p-4 rounded-md cursor-pointer hover:bg-green-100"
+            onClick={() => onSelectCard(card)}
+          >
+            <h3 className="text-lg font-semibold">{card.title}</h3>
+            <p className="text-gray-500">{card.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
