7c7b2e27b991f86561e65cb9a2f5313584593f0f
tommoko3ds <kenyaescamill@gmail.com>
2024-02-27 22:41:26 -0500

    tercer commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-export default function App() {
-  return (
-    <h1 className="text-4xl font-bold text-pink-300">
-      Hello world!
-    </h1>
-  )
-}
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="flex flex-col items-start justify-center h-screen p-8">
+            <h1 className="text-3xl font-bold text-gray-800">Bienvenido (Dirección)</h1>
+            <div className=''>
+            <br>
+            </br>
+            <br>
+            </br>
+            <br>
+            </br>
+            <div className="bg-gray-100 rounded-lg shadow-lg p-12 mt-8">
+                <h2 className="text-xl font-semibold text-gray-800">Reportes</h2>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
